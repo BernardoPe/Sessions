52b478cf3f607a409c854a83eb3299a6e41b7599
Bernardo <tgavent3@gmail.com>
2024-02-28 17:48:59 +0000

    Database tests: create and delete test tables, added insert test, changed select test

--- src/test/kotlin/pt/isel/ls/utils/DBTests.kt
+++ src/test/kotlin/pt/isel/ls/utils/DBTests.kt
@@ -5,8 +5,6 @@ import org.junit.Before
 import org.junit.Test
 import org.postgresql.ds.PGSimpleDataSource
 
-import kotlin.test.assertTrue
-
 class DBTests {
 
     val dataSource = PGSimpleDataSource()
