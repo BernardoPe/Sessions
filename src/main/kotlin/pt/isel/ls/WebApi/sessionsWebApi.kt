9ba10c93b7eb98724b6b42b59e466c3abfa6ea7a
Bernardo <tgavent3@gmail.com>
2024-03-06 20:50:28 +0000

    Start of server and api implementations

--- src/main/kotlin/pt/isel/ls/WebApi/sessionsWebApi.kt
+++ src/main/kotlin/pt/isel/ls/WebApi/sessionsWebApi.kt
@@ -1,4 +1,96 @@
 package pt.isel.ls.WebApi
 
-interface sessionsWebApi {
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.slf4j.Logger
+import pt.isel.ls.Services.*
+
+class SessionsApi(val logger: Logger,
+                  val playerServices: playerService,
+                  val gameServices: gameService,
+                  val sessionServices: sessionsService
+) {
+
+    private val processors = mapOf(
+        "createPlayer" to ::createPlayer,
+        "getPlayerDetails" to ::getPlayerDetails,
+        "createGame" to ::createGame,
+        "getGameDetails" to ::getGameDetails,
+        "getGameList" to ::getGameList,
+        "createSession" to ::createSession,
+        "addPlayerToSession" to ::addPlayerToSession,
+        "getSessionDetails" to ::getSessionDetails,
+        "getSessionList" to ::getSessionList,
+    )
+
+    private fun createPlayer(request: Request): Response {
+        TODO()
+    }
+
+    private fun getPlayerDetails(request: Request): Response {
+        TODO()
+    }
+
+    private fun createGame(request: Request): Response {
+        TODO()
+    }
+
+    private fun getGameDetails(request: Request): Response {
+        TODO()
+    }
+
+    private fun getGameList(request: Request): Response {
+        TODO()
+    }
+
+    private fun createSession(request: Request): Response {
+        TODO()
+    }
+
+    private fun addPlayerToSession(request: Request): Response {
+        TODO()
+    }
+
+    private fun getSessionDetails(request: Request): Response {
+        TODO()
+    }
+
+    private fun getSessionList(request: Request): Response {
+        TODO()
+    }
+
+    fun processRequest(request: Request, service: String, requiresAuth: Boolean): Response {
+
+        logRequest(request)
+
+        if (requiresAuth)
+            verifyAuth(request)
+
+        return try {
+            val processFunction = processors[service]!!
+            processFunction(request)
+        } catch (e: Exception) {
+            // TODO ERROR HANDLING
+            Response(OK).header("content-type", "text/plain").body(e.message ?: "Internal Server Error")
+        }
+
+    }
+
+    private fun logRequest(request: Request) {
+        logger.info(
+            "incoming request: method={}, uri={}, content-type={} accept={}",
+            request.method,
+            request.uri,
+            request.header("content-type"),
+            request.header("accept"),
+        )
+    }
+
+    private fun verifyAuth(request: Request) {
+        TODO()
+    }
+
+
 }
+
--- src/main/kotlin/pt/isel/ls/sessionsServer.kt
+++ src/main/kotlin/pt/isel/ls/sessionsServer.kt
@@ -1,95 +1,55 @@
 package pt.isel.ls
 
-import kotlinx.datetime.Clock
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import org.http4k.core.Method.GET
-import org.http4k.core.Method.POST
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.CREATED
-import org.http4k.core.Status.Companion.OK
+import org.http4k.core.Method.*
 import org.http4k.routing.bind
-import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.slf4j.LoggerFactory
+import pt.isel.ls.Services.gameService
+import pt.isel.ls.Services.playerService
+import pt.isel.ls.Services.sessionsService
+import pt.isel.ls.WebApi.*
 
-private val logger = LoggerFactory.getLogger("pt.isel.ls.http.HTTPServer")
-
-@Serializable
-data class Student(val name: String, val number: Int)
-
-val students =
-    mutableListOf(
-        Student("Filipe", 10),
-        Student("Luis", 20),
-        Student("Daniel", 30),
-    )
-
-fun getStudents(request: Request): Response {
-    logRequest(request)
-    val limit = request.query("limit")?.toInt() ?: 2
-    return Response(OK)
-        .header("content-type", "application/json")
-        .body(Json.encodeToString(students.take(limit)))
-}
-
-fun getStudent(request: Request): Response {
-    logRequest(request)
-    val stdNumber = request.path("number")?.toInt()
-    return Response(OK)
-        .header("content-type", "application/json")
-        .body(Json.encodeToString(students.find { it.number == stdNumber }))
-}
-
-fun postStudent(request: Request): Response {
-    logRequest(request)
-    val std = Json.decodeFromString<Student>(request.bodyString())
-    students.add(std)
-    return Response(CREATED)
-        .header("content-type", "application/json")
-        .body(Json.encodeToString(std))
-}
-
-fun getDate(request: Request): Response {
-    return Response(OK)
-        .header("content-type", "text/plain")
-        .body(Clock.System.now().toString())
-}
-
-fun logRequest(request: Request) {
-    logger.info(
-        "incoming request: method={}, uri={}, content-type={} accept={}",
-        request.method,
-        request.uri,
-        request.header("content-type"),
-        request.header("accept"),
-    )
-}
+val logger = LoggerFactory.getLogger("pt.isel.ls.http.HTTPServer")
 
 fun main() {
-    val studentRoutes =
+
+    val requestProcessor = SessionsApi(logger, playerService(), gameService(), sessionsService())::processRequest
+
+    val playerRoutes =
         routes(
-            "students" bind GET to ::getStudents,
-            "students/{number}" bind GET to ::getStudent,
-            "students" bind POST to ::postStudent,
+            "players" bind POST to { req -> requestProcessor(req, "createPlayer", false) },
+            "players/{pid}" bind GET to { req -> requestProcessor(req, "getPlayerDetails", false) }
+        )
+
+    val gameRoutes =
+        routes(
+            "games" bind POST to { req -> requestProcessor(req, "createGame", true) },
+            "games/{gid}" bind GET to { req -> requestProcessor(req, "getGameDetails", false) },
+            "games" bind GET to { req -> requestProcessor(req, "getGameList", false) }
+        )
+
+    val sessionRoutes =
+        routes(
+            "sessions" bind POST to { req -> requestProcessor(req, "createSession", true) },
+            "sessions/{ssid}" bind PUT to { req -> requestProcessor(req, "addPlayerToSession", true) },
+            "sessions/{ssid}" bind GET to { req -> requestProcessor(req, "getSessionDetails", false) },
+            "sessions/{gid}/list" bind GET to { req -> requestProcessor(req, "getSessionList", false) }
         )
 
     val app =
         routes(
-            studentRoutes,
-            "date" bind GET to ::getDate,
+            playerRoutes,
+            gameRoutes,
+            sessionRoutes
         )
 
     val jettyServer = app.asServer(Jetty(8080)).start()
-    logger.info("server started listening")
+    logger.info("Server started listening")
 
     readln()
     jettyServer.stop()
 
-    logger.info("leaving Main")
+    logger.info("Leaving Main")
 }
