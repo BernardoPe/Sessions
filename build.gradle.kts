dacbd15f91a415c70e2349bfa6ccd6ee172a09f6
Initdd <a50468@alunos.isel.pt>
2024-03-06 16:28:30 +0000

    Added the example code for the server and installed the libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,12 @@ repositories {
 
 dependencies {
     implementation(group = "org.postgresql", name = "postgresql", version = "42.+")
+    implementation(group = "org.http4k", name = "http4k-core", version = "4.40.+")
+    implementation(group = "org.http4k", name = "http4k-server-jetty", version = "4.40.+")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.5.+")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-datetime", version = "0.4.+")
+    implementation(group = "org.slf4j", name = "slf4j-api", version = "2.0.0-alpha5")
+    runtimeOnly(group = "org.slf4j", name = "slf4j-simple", version = "2.0.0-alpha5")
     testImplementation(kotlin("test"))
 }
 
