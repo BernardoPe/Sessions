44058ecd969a3d4096779cd360a2a90eb6c00b9d
Initdd <a50468@alunos.isel.pt>
2024-03-11 21:02:00 +0000

    Commented the SessionsData interface

--- src/main/kotlin/pt/isel/ls/Storage/SessionsData.kt
+++ src/main/kotlin/pt/isel/ls/Storage/SessionsData.kt
@@ -1,11 +1,44 @@
 package pt.isel.ls.Storage
 
+/**
+ *  Sessions Data management interface
+ *
+ *  This interface is used to manage the data of the sessions
+ *  It is used to create, read, update and delete items from the database
+ *
+ *  @property create Create a new item in the database
+ *  @property read Read an item from the database
+ *  @property update Update an item in the database
+ *  @property delete Delete an item from the database
+ */
 interface SessionsData<E> {
+    /**
+     * Create a new item in the database
+     *
+     * @param value The item to be created
+     */
     fun create(value: E)
 
+    /**
+     * Read an item from the database by its identifier
+     *
+     * @param id The item identifier
+     * @return The item with the given id or null if it does not exist
+     */
     fun read(id: Int): E?
 
+    /**
+     * Update an item in the database
+     *
+     * @param id The item identifier
+     * @param value The new item
+     */
     fun update(id: Int, value: E)
 
+    /**
+     * Delete an item from the database
+     *
+     * @param id The item identifier
+     */
     fun delete(id: Int)
 }
