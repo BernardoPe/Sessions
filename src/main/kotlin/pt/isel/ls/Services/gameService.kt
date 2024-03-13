b2307f91aa3a6c7c371fb436223021fa91122d75
Initdd <a50468@alunos.isel.pt>
2024-03-13 17:12:46 +0000

    Started filling gameService (not much)

--- src/main/kotlin/pt/isel/ls/Services/gameService.kt
+++ src/main/kotlin/pt/isel/ls/Services/gameService.kt
@@ -1,9 +1,16 @@
 package pt.isel.ls.Services
 
-class gameService {
+import pt.isel.ls.Storage.SessionsData
+import pt.isel.ls.DTO.Game.Game
 
-    fun createGame() {
-        TODO()
+
+class gameService(val storage: SessionsData<Game>) {
+
+    fun createGame(game: Game) {
+        storage.create(game)
+    }
+    fun getGameById(id: Int) {
+        val game = storage.get(id)
     }
 
     fun detailsGame() {
