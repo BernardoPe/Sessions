b92e46db24704c39a285c06474a6ff9bf6caa2ae
Initdd <a50468@alunos.isel.pt>
2024-03-13 15:59:12 +0000

    Created the Exception objects: Web Api Errors and Services Errors

---
+++ src/main/kotlin/pt/isel/ls/Exceptions/WebExceptions.kt
@@ -0,0 +1,22 @@
+package pt.isel.ls.Exceptions
+
+sealed class WebExceptions(status: Int, description: String, cause: String?) : Exception() {
+    val status: Int = status
+    val description: String = description
+    val error_cause: String? = cause
+}
+
+// 400 Bad Request
+class BadRequestException(cause: String?) : WebExceptions(400, "Bad Request", cause)
+
+// 404 Not Found
+class NotFoundException(item: String?) : WebExceptions(404, "Not Found", item)
+
+// 500 Internal Server Error
+class InternalErrorException() : WebExceptions(500, "Internal Server Error", null)
+
+// 501 Not Implemented
+class NotImplementedException() : WebExceptions(501, "Not Implemented", null)
+
+
+
