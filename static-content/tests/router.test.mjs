3706601c9c8203213cf5986935af0f1cbdced0f1
Initdd <a50468@alunos.isel.pt>
2024-04-10 14:59:30 +0100

    added tests to the router, and cleaned both this test and the npm package-lock.json

--- static-content/tests/router.test.mjs
+++ static-content/tests/router.test.mjs
@@ -3,24 +3,24 @@ import router from "../router.js";
 
 describe('Router', () => {
     beforeEach(() => {
-        router.addRouteHandler('/test', () => 'Test handler')
-        router.addDefaultNotFoundRouteHandler(() => 'Not found handler')
+        router.addRouteHandler('/test', () => 'Test handler');
+        router.addDefaultNotFoundRouteHandler(() => 'Not found handler');
     })
 
     afterEach(() => {
         // Reset routes and handlers after each test
-        router.routes = []
-        router.notFoundRouteHandler = () => { throw "Route handler for unknown routes not defined" }
+        router.routes = [];
+        router.notFoundRouteHandler = () => { throw "Route handler for unknown routes not defined" };
     })
 
     it('returns correct handler for existing route', () => {
-        let routehandler = router.getRouteHandler('/test')
-        routehandler().should.equal('Test handler')
+        let routehandler = router.getRouteHandler('/test');
+        routehandler().should.equal('Test handler');
     })
 
     it('returns not found handler for non-existing route', () => {
-        let routehandler = router.getRouteHandler('/unknown')
-        routehandler().should.equal('Not found handler')
+        let routehandler = router.getRouteHandler('/unknown');
+        routehandler().should.equal('Not found handler');
     })
 
     it('returns correct parameters and query for route with parameters and query', () => {
@@ -35,4 +35,18 @@ describe('Router', () => {
         (Object.keys(params).length).should.equal(0);
         (Object.keys(query).length).should.equal(0);
     })
+
+    it('returns empty parameters and query for route with parameters but without query', () => {
+        router.addRouteHandler('/user/:id', () => 'User handler');
+        const { params, query } = router.getRequestParamsAndQuery('/user/123');
+        params.id.should.equal('123');
+        (Object.keys(query).length).should.equal(0);
+    })
+
+    it('returns empty parameters and query for route without parameters but with query', () => {
+        const { params, query } = router.getRequestParamsAndQuery('/test?active=true');
+        (Object.keys(params).length).should.equal(0);
+        query.active.should.equal('true');
+    })
+
 })
